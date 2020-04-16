e333461164178308fc574fac620c1a792d9c4bb4
Jelle <jellegerbrandy@gmail.com>
2020-04-16 16:28:57 +0200

    finish createCommon script

--- createCommon.js
+++ createCommon.js
@@ -8,10 +8,10 @@ const {getForgeOrgData, getSetSchemesData } = require('@daostack/common-factory'
 const OVERRIDES =  { gasLimit: 7500000, value: 0}
 
 async function createCommon() {
+  const DAONAME = `Test DAO ${Math.floor(Math.random() * 100000)}`
   let tx;
   let receipt
   const { arc, wallet } = await getArcAndWallet();
-
   const daoFactoryAbi = require(path.join(pathToABIs, 'DAOFactory.json')).abi
   // getContract does not work the current client version, cf https://github.com/daostack/client/issues/445
   // const contract = arc.getContract(requestToJoinSchemeState.address, abi)
@@ -19,31 +19,26 @@ async function createCommon() {
   console.log(`using DAOFactory instance @ ${CONTRACT_ADDRESSES.DAOFactoryInstance}`)
   const daoFactoryContract = new ethers.Contract(CONTRACT_ADDRESSES.DAOFactoryInstance, daoFactoryAbi, wallet)
 
+  console.log(`Calling DAOFactory.forgeOrg(...)`)
   const forgeOrgData = 
       getForgeOrgData({
           DAOFactoryInstance: CONTRACT_ADDRESSES.DAOFactoryInstance,
-          orgName: 'Created by CommonScripts',
+          orgName: DAONAME,
           founderAddresses: [WALLET_ADDRESS],
           repDist: [100]
       })
-  // console.log('FORGE ORG DATA: ', forgeOrgData);
-  console.log(`Calling DAOFactory.forgeOrg(...)`)
   tx = await daoFactoryContract.forgeOrg(...forgeOrgData, OVERRIDES)
   receipt = await tx.wait()
-  // console.log(receipt)
   // get the new avatar address of the thing that was just created..
   const newOrgEvent = receipt.events.filter((e) => e.event === 'NewOrg')[0]
-  // console.log(newOrgEvent)
   const newOrgAddress = newOrgEvent.args['_avatar']
 
-
   console.log(`Calling DAOFactory.setSchemes(...)`)
-  
   // TODO: Use proper IPFS hash
   let ipfsHash = 'metaData'
   // deadline in Ethereum time, where 1 unit = 1 second (I think)
-  const deadLine = (await arc.web3.getBlock('latest')).timestamp + 3000
-  console.log(deadLine)
+  const deadline = (await arc.web3.getBlock('latest')).timestamp + 3000
+  // console.log(deadline)
   const schemeData = getSetSchemesData({
       DAOFactoryInstance: CONTRACT_ADDRESSES.DAOFactoryInstance,
       avatar: newOrgAddress,
@@ -52,14 +47,13 @@ async function createCommon() {
       minFeeToJoin: 100,
       memberReputation: 100,
       goal: 1000,
-      deadLine,
+      deadline,
       metaData: ipfsHash,
     })
 
-  console.log('SCHEME DATA: ', schemeData);
   tx = await daoFactoryContract.setSchemes(...schemeData, OVERRIDES)
   receipt = await tx.wait()
-  console.log(receipt)
+  console.log(`Created a DAO at ${newOrgAddress} with name "${DAONAME}"`)
 
   process.exit(0)
 };
